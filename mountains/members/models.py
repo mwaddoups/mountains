3f102872a5dc9cc13066b136cd8d97a0019cf6da
Mark Waddoups <mark.waddoups@bayesianshift.co.uk>
2021-12-08 09:42:49 +0000

    First commit, basic user model

---
+++ mountains/members/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    pass
