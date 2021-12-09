390a3eb781a677c6ee89be940e29f4bd286564ff
Mark Waddoups <mark.waddoups@bayesianshift.co.uk>
2021-12-09 13:52:54 +0000

    Got navbar looking better

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -3,20 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <nav className="flex items-center p-6 border-b-4 text-gray-900">
-      <div className="mr-12">
+    <nav className="container flex items-center border-b-4 text-gray-900">
+      <div className="ml-4 mr-12">
         <span className="font-semibold text-xl tracking-tight">
           Clyde Mountaineering Club
         </span>
       </div>
-      <ul>
-        <li className="mr-6 inline-block">
-          <Link to="/" className="hover:text-blue-300">Home</Link>
-        </li>
-        <li className="mr-6 inline-block">
-          <Link to="/members" className="hover:text-blue-300">Members Area</Link>
-        </li>
-      </ul>
+      <Link to="/" className="p-4 hover:bg-gray-200">Home</Link>
+      <Link to="/members" className="p-4 hover:bg-gray-200">Members Area</Link>
     </nav>
   )
 }
