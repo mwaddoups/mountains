16937fb5157bd2980127856f9ef3082f70aec9b1
Mark Waddoups <mwaddoups@gmail.com>
2021-12-13 11:06:25 +0000

    Made social app in django

---
+++ mountains/social/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.db.models.fields.related import ForeignKey
+from members.models import User, Event
+
+class FeedPost(models.Model):
+    user_id = ForeignKey(User, on_delete=models.SET_NULL, blank=False, null=True)
+    posted = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(blank=False)
+
+class EventPost(FeedPost):
+    event_id = ForeignKey(Event, on_delete=models.CASCADE)
+
+class FeedComment(models.Model):
+    post_id = ForeignKey(FeedPost, on_delete=models.CASCADE)
+    user_id = ForeignKey(User, on_delete=models.SET_NULL, blank=False, null=True)
+    posted = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(blank=False)
