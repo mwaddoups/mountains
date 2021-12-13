ab2adcbe9dbbd3ce61c2c4a83b4760aa6eb779e3
Mark Waddoups <mwaddoups@gmail.com>
2021-12-13 09:14:29 +0000

    Set up basic api calls for members

--- frontend/src/components/Members.tsx
+++ frontend/src/components/Members.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../api";
+import { User } from "../models";
 
 export default function Members() {
-  return <h2>Members List</h2>  
+  const [userList, setUserList] = useState<Array<User>>([])
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    api.get("/users").then(response => {
+      setUserList(response.data);
+      setIsLoading(false);
+    });
+  }, [setIsLoading, setUserList])
+
+  return (
+    isLoading
+    ? <h2>Loading...</h2>
+    : <h2>{userList.map(user => <p key={user.id}>{JSON.stringify(user)}</p>)}</h2>
+  )
 }
---
+++ frontend/src/models.ts
@@ -0,0 +1,6 @@
+export type User = {
+  id: number,
+  first_name: string,
+  last_name: string,
+  phone_number: string,
+}
