2e6f92b7696861d68be98cd2a19f677c05866ed2
Mark Waddoups <mwaddoups@gmail.com>
2021-12-14 16:56:47 +0000

    Improved flow for logging in

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -8,13 +8,22 @@ interface LoginProps {
 export default function Login({ setAuthToken }: LoginProps) {
   const [email, setEmail] = useState<string>('');
   const [token, setToken] = useState<string>('');
+  const [waitingForToken, setWaitingForToken] = useState(false);
 
+  const requestToken = useCallback(() => {
+    console.log('Sending token request...');
+    api.post(
+      'auth/email/', { email }
+    ).then(response => {
+      console.log(response);
+      setWaitingForToken(true);
+    }).catch(res => console.log(res))
+  }, [email, setWaitingForToken]);
   
   const sendLogin = useCallback(() => {
     console.log('Sending login request...');
-    let endpoint = token ? 'auth/token/' : 'auth/email/'
     api.post(
-      endpoint, { email, token }
+      'auth/token/', { email, token }
     ).then(response => {
       setAuthToken(response.data.token);
     }).catch(res => console.log(res))
@@ -24,9 +33,13 @@ export default function Login({ setAuthToken }: LoginProps) {
     <form onSubmit={e => e.preventDefault()}>
       <label htmlFor="email">Email</label>
       <input type="email" id="email" value={email} onChange={event => setEmail(event.target.value)}/>
-      <label htmlFor="password">Token</label>
-      <input type="password" id="password" value={token} onChange={event => setToken(event.target.value)} />
-      <button onClick={sendLogin}>Login</button>
+      {waitingForToken && (
+        <>
+        <label htmlFor="token">Token (check email)</label>
+        <input type="number" id="token" value={token} onChange={event => setToken(event.target.value)} />
+        </>
+      )}
+      <button onClick={waitingForToken ? sendLogin : requestToken}>Login/Signup</button>
     </form>
   )
 }
