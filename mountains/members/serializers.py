9c8e567b5501dfb733aca2dd847ac721ff478eec
Mark Waddoups <mwaddoups@gmail.com>
2022-01-05 12:06:11 +0000

    Made the SmallUserSerializer

--- mountains/members/serializers.py
+++ mountains/members/serializers.py
@@ -3,14 +3,19 @@ from rest_framework_nested.serializers import NestedHyperlinkedModelSerializer
 from .models import User, Experience
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-  class Meta:
-    model = User
-    fields = ['id', 'first_name', 'last_name', 'mobile_number', 'email']
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'mobile_number', 'email']
+
+class SmallUserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name']
 
 class ExperienceSerializer(NestedHyperlinkedModelSerializer):
-  parent_lookup_kwargs = {
-    'user_pk': 'user__pk'
-  }
-  class Meta:
-    model = Experience
-    fields = '__all__'
+    parent_lookup_kwargs = {
+        'user_pk': 'user__pk'
+    }
+    class Meta:
+        model = Experience
+        fields = '__all__'
--- mountains/social/serializers.py
+++ mountains/social/serializers.py
@@ -1,10 +1,16 @@
-from members.serializers import UserSerializer
-from members.models import User
+from members.serializers import SmallUserSerializer
 from rest_framework import serializers
 from .models import FeedPost, Comment
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = SmallUserSerializer(read_only=True)
+
+    def create(self, validated_data):
+        # Use authenticated user to author the comment
+        return FeedPost.objects.create(
+            user=self.context['request'].user,
+            **validated_data
+        )
     class Meta:
       model = Comment
       fields = ['id', 'post_id', 'user', 'posted', 'text']
@@ -13,16 +19,15 @@ class CommentSerializer(serializers.HyperlinkedModelSerializer):
 
 class FeedPostSerializer(serializers.HyperlinkedModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
-    user = UserSerializer(read_only=True)
+    user = SmallUserSerializer(read_only=True)
 
     def create(self, validated_data):
         # Use authenticated user to author the post
-
         return FeedPost.objects.create(
             user=self.context['request'].user,
             **validated_data
         )
-        
+
     class Meta:
         model = FeedPost
         fields = ['id', 'user', 'posted', 'text', 'comments']
