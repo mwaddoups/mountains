8c7a381dccd6ea456a72a9a8a59580843a73c7ad
Mark Waddoups <mwaddoups@gmail.com>
2022-01-07 13:06:12 +0000

    Pulled out ProfileSquare component

--- frontend/src/components/Members.tsx
+++ frontend/src/components/Members.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import api from "../api";
-import { getName } from "../methods/user";
 import { User } from "../models";
 import Loading from "./Loading";
-import ProfilePicture from "./ProfilePicture";
+import ProfileSquare from "./ProfileSquare";
 
 export default function Members() {
   const [userList, setUserList] = useState<Array<User>>([])
@@ -24,24 +22,5 @@ export default function Members() {
         {userList.map(user => <ProfileSquare key={user.url} user={user} />)}
       </div>
     </Loading>
-  )
-}
-
-interface ProfileSquareProps {
-  user: User,
-}
-function ProfileSquare({user}: ProfileSquareProps) {
-  return (
-    <Link to={`${user.id}`}>
-      <div className="w-48 h-48 rounded-lg p-2">
-        <div className="relative z-0 mx-auto w-5/6">
-          <ProfilePicture imageUrl={user.profile_picture} />
-        </div>
-        <div className="relative border border-gray-300 mx-auto text-center -mt-3 bg-white rounded z-50">
-          <h1 className="text-lg font-semibold">{getName(user)}</h1> 
-        </div>
-
-      </div>
-    </Link>
   )
 }
---
+++ frontend/src/components/ProfileSquare.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { getName } from "../methods/user";
+import { User } from "../models";
+import ProfilePicture from "./ProfilePicture";
+
+interface ProfileSquareProps {
+  user: User,
+}
+
+export default function ProfileSquare({user}: ProfileSquareProps) {
+  return (
+    <Link to={`${user.id}`} className="inline-block w-48 h-48 p-2 shadow m-2">
+      <div className="relative z-0 mx-auto w-3/4">
+        <ProfilePicture imageUrl={user.profile_picture} />
+      </div>
+      <div className="relative mx-auto text-center mt-2 bg-white rounded z-50">
+        <h1 className="font-light">{getName(user)}</h1> 
+      </div>
+    </Link>
+  )
+}
