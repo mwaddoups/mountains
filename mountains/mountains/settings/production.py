2b7cb1b7cc18bcdc9fa2033d2aff427a2727a606
Mark Waddoups <mark.waddoups@bayesianshift.co.uk>
2022-01-13 14:37:14 +0000

    Updated to use postgresql in prod

--- mountains/mountains/settings/production.py
+++ mountains/mountains/settings/production.py
@@ -6,7 +6,7 @@ SESSION_COOKIE_SECURE = True
 
 CORS_ALLOWED_ORIGINS = []
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['clydemc.org', 'www.clydemc.org', 'localhost']
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
@@ -15,8 +15,14 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'HOST': 'localhost',
+            'PORT': 5432,
+            'NAME': 'mountains',
+            'USER': os.environ['PG_USER'],
+            'PASSWORD': os.environ['PG_PASS'],
+        }
     }
 }
 
