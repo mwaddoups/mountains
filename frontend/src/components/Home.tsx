7d4b80244d261fb303e4a5f7a15c9b365bf1988a
Mark Waddoups <mark.waddoups@bayesianshift.co.uk>
2022-01-19 11:52:11 +0000

    Updated text on landing and homepage

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,30 +2,55 @@ import React from "react";
 import tw from "twin.macro";
 
 const Paragraph = tw.p`my-3 ml-3 tracking-wide font-light`
+const Section = tw.div`rounded shadow p-4 mb-4`
+const Heading = tw.h1`text-3xl font-bold tracking-tight`
 
 export default function Home() {
   return (
     <div>
-      <div className="rounded shadow p-4 mb-4">
-        <h1 className="text-3xl font-bold tracking-tight">Welcome to Clyde Mountaineering Club!</h1>
+      <Section>
+        <Heading>Welcome to Clyde Mountaineering Club!</Heading>
         <Paragraph>
-          Our club operates mainly on this site, and on our Discord server.
-          There is an invite link to the discord server here.
+          Our club operates on both this site, and on our Discord server. If you haven't used Discord before, 
+          it's similar to Whatsapp but allows us to have multiple groups for different chats (avoiding the chaos)!
+          You can download the app both on your phone and on your laptop.
         </Paragraph>
+        <Paragraph>Join our discord server here!</Paragraph>
         <Paragraph>
           You can see our events, members and other information using the sidebar on the side.
         </Paragraph>
         <Paragraph>
-          You can adjust or update your profile using your profile link at the top of the page.
+          You can adjust or update your profile using your profile link at the top of the page. Please add a picture 
+          and feel free to add your mountaineering experience as well! We're hoping that this will make it easier to find
+          similar members in future.
         </Paragraph>
-      </div>
-      <div id="feedback" className="rounded shadow p-4 mb-4">
-        <h1 className="text-3xl font-bold tracking-tight">The website is a work-in-progress...</h1>
+      </Section>
+      <Section id="walks">
+        <Heading>Getting outside!</Heading>
         <Paragraph>
-          The website is still very new and we are actively looking for feedback. Let us know about
-          anything, big or small, that you'd like to see added here.
+          As the club is still growing and conditions are (somewhat) wintery, we are unlikely to run official walks until we return to
+          summer conditions. However, hopefully there are plenty of people out there who are keen to get out with others and do some winter
+          walking. 
         </Paragraph>
-      </div>
+        <Paragraph>
+          If you want to get out, just post up either in Discord or on this site (on the feed) and say where you fancy going and if you have a car. Hopefully other 
+          members will then see your post and you should be able to fill seats, split petrol and everything else.
+        </Paragraph>
+        <Paragraph>
+          Please note that any events organized in this way 
+        </Paragraph>
+      </Section>
+      <Section id="feedback">
+        <Heading>The club and website is a work-in-progress...</Heading>
+        <Paragraph>
+          The club is still very much a work-in-progress. Any ideas for our future or ways to plan ahead? Let us know in the discord on the #feedback channel.
+        </Paragraph>
+        <Paragraph>
+          The website is also still very new and we are actively looking for feedback. Let us know about
+          anything, big or small, that you'd like to see added by either posting up on our Feed here, or
+          by chatting in the Discord #website channel.
+        </Paragraph>
+      </Section>
     </div>
   )
 }
--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Landing() {
   return (
     <main className="min-h-full">
       <section className="bg-landing bg-cover bg-bottom bg-fixed h-80"></section>
       <Section header="About Us">
-        <Paragraph>We are a mountaineering club based in Glasgow and founded in 2021.  We were created to focus on the 20s and 30s age groups within the city, but are open to all. Our aim is to create a group of friendly, kind and like-minded people who like to get out and have challenging days in the mountains, safely.</Paragraph>
+        <Paragraph>We are a mountaineering club based in Glasgow and founded in 2022.  We were created to focus on the 20s and 30s age groups within the city, but are open to all. Our aim is to create a group of friendly, kind and like-minded people who like to get out and have challenging days in the mountains, safely.</Paragraph>
         <Paragraph>Our main activities are harder hill and mountain walking, winter walking, scrambling and easy climbing - together with other outdoor activities in Scotland such as skiing and trail running.</Paragraph>
-        <Paragraph>We try to run day trips at least every other weekend, and have weekend trips away to further afield locations. We also have an active social side in the city, with pub trips and indoor climbing/bouldering.</Paragraph>
+        <Paragraph>We try to run official day trips around every other weekend, and have weekend trips away to further afield locations. We also have an active social side in the city, with pub trips and indoor climbing/bouldering.</Paragraph>
+        <Paragraph>The club is still growing and most activities are currently informal between members. We are hoping to start club-backed walks in Spring 2022.</Paragraph>
       </Section>
       <Section header="Join Us">
-        <Paragraph>In order to join as a new member...</Paragraph>
+        <Paragraph>If you are interested in joining, the first step is <span className="text-blue-600 hover:text-blue-400"><Link to="/platform">registering on our site.</Link></span> Once you are approved, you will have access to our internal site and our groups.</Paragraph>
+        <Paragraph>As the club is still developing, we have yet to introduce official membership - but will be adding paid annual membership later on, with concessions for those who need it. Get involved now to have your say in how the club develops!</Paragraph>
         <Paragraph>Any other questions or want to say hello? Get in touch at hello@clydemc.co.uk</Paragraph>
       </Section>
       <Section header="Upcoming Events">
-        <Paragraph>Placeholder for the next 5 events</Paragraph>
+        <Paragraph>Once we start to run official events, you will see these here.</Paragraph>
       </Section>
       <Section header="Photos">
-        <Paragraph>Placeholder for recent photo gallery</Paragraph>
+        <Paragraph>Club photos will eventually appear here - the website (and club) is still a work in progress!</Paragraph>
       </Section>
     </main>
   )
