85bdeaa71d4fe64270937bf47ff5a42f43f44cd8
Mark Waddoups <mark.waddoups@bayesianshift.co.uk>
2022-01-19 14:58:56 +0000

    Removed sessionauthentication as option for rest framework

--- mountains/mountains/settings/base.py
+++ mountains/mountains/settings/base.py
@@ -51,7 +51,6 @@ AUTH_USER_MODEL = 'members.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAdminUser',
