99fbe6aceea5d0b53e1985ad9326382940c37f85
Mark Waddoups <mark.waddoups@bayesianshift.co.uk>
2022-01-19 10:46:18 +0000

    Added normalized rotation

--- mountains/mountains/settings/base.py
+++ mountains/mountains/settings/base.py
@@ -148,3 +148,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Image settings
+DJANGORESIZED_DEFAULT_NORMALIZE_ROTATION = False
