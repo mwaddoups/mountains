8320f68fc309447fde38e31e5aabb15e25904953
Mark Waddoups <mark.waddoups@bayesianshift.co.uk>
2022-01-25 13:51:33 +0000

    Updated to use 5 users

--- frontend/src/components/members/Members.tsx
+++ frontend/src/components/members/Members.tsx
@@ -8,7 +8,7 @@ import ProfileSquare from "./ProfileSquare";
 
 export default function Members() {
   // Fill with fake users for the first load
-  const [userList, setUserList] = useState<Array<User>>(Array(2).fill(
+  const [userList, setUserList] = useState<Array<User>>(Array(5).fill(
     {id: 1, url: '', first_name: 'User', last_name: '', profile_picture: undefined, is_approved: true},
   ))
   const [isLoading, setIsLoading] = useState(true);
