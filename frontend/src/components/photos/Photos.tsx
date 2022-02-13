a475ea5126b7b02438e3a9468853ef2cd4534f23
Mark Waddoups <mwaddoups@gmail.com>
2022-02-13 19:32:37 +0000

    Got fullscreen working

--- frontend/src/components/photos/Photos.tsx
+++ frontend/src/components/photos/Photos.tsx
@@ -7,6 +7,7 @@ import PhotoUploader from "./PhotoUploader";
 export default function Photos() {
   const [needsRefresh, setNeedsRefresh] = useState(true);
   const [photos, setPhotos] = useState<Array<Photo>>([]);
+  const [highlightedPhoto, setHighlightedPhoto] = useState<number | null>(null);
 
   useEffect(() => {
     api.get('photos/').then(res => {
@@ -24,13 +25,35 @@ export default function Photos() {
       </div>
       <Loading loading={needsRefresh}>
         <div className="flex flex-wrap">
-          {photos.map(photo => (
-            <img 
-              className="w-40 p-2 flex-1"
-              key={photo.id} src={photo.photo} alt="Trip" />
-          ))}
+          {photos.map((photo, ix) => <GalleryPhoto 
+            key={photo.id} photo={photo} 
+            onClick={() => setHighlightedPhoto(ix)} />)}
         </div>
       </Loading>
+      {(highlightedPhoto !== null) && (
+        <div 
+          onClick={() => setHighlightedPhoto(null)}
+          className="fixed inset-0 w-full h-screen bg-black flex justify-center align-center">
+          <img 
+            className="p-4 rounded w-full object-contain"
+            src={photos[highlightedPhoto].photo} alt="Mountains" />
+        </div>
+      )}
     </div>
+  )
+}
+
+
+interface GalleryPhotoProps {
+  photo: Photo,
+  onClick: any
+}
+
+function GalleryPhoto({ photo, onClick }: GalleryPhotoProps) {
+  return (
+    <img 
+      className="w-40 p-2 flex-1"
+      onClick={onClick}
+      src={photo.photo} alt="Mountains" />
   )
 }
