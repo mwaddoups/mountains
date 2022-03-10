15b2080bdea74dd0acdc5734faeb24a067956a82
Mark Waddoups <mwaddoups@gmail.com>
2022-03-10 08:02:18 +0000

    Wrote ClydeMarkdown component

---
+++ frontend/src/components/ClydeMarkdown.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import tw from "twin.macro";
+
+const MainHeading = tw.h1`text-3xl mb-2 font-bold tracking-tight`
+const Paragraph = tw.p`mb-2 font-light text-sm`
+const UList = tw.ul`list-disc ml-6 mb-2 text-sm font-light tracking-wide`
+const OList = tw.ol`list-decimal ml-6 mb-2 text-sm font-light tracking-wide`
+const LI = tw.li`font-light tracking-wide text-sm`
+const SubHeading = tw.h2`text-2xl mb-2 font-bold tracking-tight`
+const SmallHeading = tw.h3`text-lg mb-2 font-bold tracking-tight`
+const Bolded = tw.p`text-gray-700 font-bold italic`
+const Italic = tw.p`text-gray-700 text-xs italic`
+
+interface ClydeMarkdownProps {
+  children: React.ReactNode
+}
+
+export default function ClydeMarkdown({ children }: ClydeMarkdownProps) {
+  return children ? (
+    <ReactMarkdown
+      components={{
+        p: Paragraph,
+        ul: UList,
+        ol: OList,
+        li: LI,
+        h1: MainHeading,
+        h2: SubHeading,
+        h3: SmallHeading,
+        strong: Bolded,
+        em: Italic,
+      }}>
+      {children.toString()}
+    </ReactMarkdown>
+  ) : <></>
+}
--- frontend/src/components/KitList.tsx
+++ frontend/src/components/KitList.tsx
@@ -1,19 +1,9 @@
 import React, { useEffect, useState } from "react";
-import ReactMarkdown from "react-markdown";
 import tw from "twin.macro";
+import ClydeMarkdown from "./ClydeMarkdown";
 import kitListURL from "./KitList.md";
 
 const Section = tw.div`rounded shadow p-4 mb-4`
-
-const MainHeading = tw.h1`text-3xl mb-2 font-bold tracking-tight`
-const Paragraph = tw.p`mb-2 font-light`
-const UList = tw.ul`list-disc ml-6 mb-2 font-light tracking-wide`
-const OList = tw.ol`list-decimal ml-6 mb-2`
-const LI = tw.li`font-light tracking-wide`
-const SubHeading = tw.h2`text-2xl mb-2 font-bold tracking-tight`
-const SmallHeading = tw.h3`text-lg mb-2 font-bold tracking-tight`
-const Bolded = tw.p`text-gray-700 font-bold italic`
-const Italic = tw.p`text-gray-700 text-xs italic`
 
 export default function KitList() {
   const [kitList, setKitList] = useState('');
@@ -31,20 +21,9 @@ export default function KitList() {
 
   return (
     <Section>
-      <ReactMarkdown
-        components={{
-          p: Paragraph,
-          ul: UList,
-          ol: OList,
-          li: LI,
-          h1: MainHeading,
-          h2: SubHeading,
-          h3: SmallHeading,
-          strong: Bolded,
-          em: Italic,
-        }}>
+      <ClydeMarkdown>
         {kitList}
-      </ReactMarkdown>
+      </ClydeMarkdown>
     </Section>
   )
 }
