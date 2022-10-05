0f14b44601b7a6afb294d6e5dad12dacbea6d64e
Mark Waddoups <mwaddoups@gmail.com>
2022-10-05 12:23:27 +0100

    Added attending user serialization with the through data nested

--- mountains/events/serializers.py
+++ mountains/events/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
-from .models import Event
+from .models import Event, AttendingUser
 from members.serializers import SmallUserSerializer, SmallUserSerializerCommittee
+
+
+class AttendingUserField(serializers.RelatedField):
+  def to_representation(self, value):
+    au_data = value.filter(event=self.context['event_id']).first()
+
+    return {'is_waiting_list': au_data.is_waiting_list}
+
+class AttendingUserSerializer(serializers.HyperlinkedModelSerializer):
+  au_data = AttendingUserField(source='attendinguser_set', read_only=True)
+
+  class Meta:
+    model = SmallUserSerializerCommittee.Meta.model
+    fields = SmallUserSerializerCommittee.Meta.fields + ['au_data']
+    read_only_fields = SmallUserSerializerCommittee.Meta.read_only_fields + ['au_data']
+
 class EventSerializer(serializers.HyperlinkedModelSerializer):
   organiser = SmallUserSerializer(read_only=True)
-  attendees = SmallUserSerializerCommittee(many=True, read_only=True)
+  attendees = serializers.SerializerMethodField()
+
+  def get_attendees(self, event):
+    return AttendingUserSerializer(
+      event.attendees.all(), 
+      many=True, read_only=True, context={'event_id': event, 'request': self.context['request']}
+    ).data
 
   def create(self, validated_data):
     return Event.objects.create(
