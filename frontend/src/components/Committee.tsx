ae9521f452ec4d485ed37d723728d20202306f03
Mark Waddoups <mwaddoups@gmail.com>
2022-10-25 16:44:30 +0100

    Updated committee page to show number of paid members

--- frontend/src/components/Committee.tsx
+++ frontend/src/components/Committee.tsx
@@ -16,20 +16,25 @@ export default function Committee() {
     });
   }, [setUserList])
 
-  const numApproved = useMemo(() => {
+  let numApproved = useMemo(() => {
     return userList.filter(u => u.is_approved).length
+  }, [userList])
+
+  let numMembers = useMemo(() => {
+    return userList.filter(u => u.is_paid).length
   }, [userList])
 
   return  (
     <div>
       <Section>
         <Heading>Member Statistics</Heading>
-        <Paragraph>{numApproved} approved members!</Paragraph> 
-        <Paragraph>{userList.length - numApproved} members in sign-up but not yet approved.</Paragraph> 
+        <Paragraph>{numApproved} approved users on site!</Paragraph> 
+        <Paragraph>{numMembers} paid members!</Paragraph> 
       </Section>
       <Section>
         <Heading>Admin Info</Heading>
         <Paragraph>You should be able to create events on the event page, and edit them as a committee member.</Paragraph>
+        <Paragraph>You can add or remove users to events, or move them back and forth to the waiting list.</Paragraph>
       </Section>
     </div>
   )
