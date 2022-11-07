885372ba3bb8b5e864adacd399dbb6e2e6aeb237
Mark Waddoups <mwaddoups@gmail.com>
2022-11-07 14:47:57 +0000

    Simplified Privacy to use ClydeMarkdown

--- frontend/src/components/Privacy.tsx
+++ frontend/src/components/Privacy.tsx
@@ -1,17 +1,7 @@
 import React, { useEffect, useState } from "react";
-import ReactMarkdown from "react-markdown";
 import tw from "twin.macro";
+import ClydeMarkdown from "./ClydeMarkdown";
 import privacyPolicy from "./PrivacyPolicy.md";
-
-const MainHeading = tw.h1`text-3xl mb-2`
-const Paragraph = tw.p`whitespace-pre-line mb-2 text-sm`
-const UList = tw.ul`list-disc ml-6 mb-2`
-const OList = tw.ol`list-decimal ml-6 mb-2`
-const LI = tw.li`text-sm`
-const SubHeading = tw.h2`text-2xl mb-2 font-bold`
-const SmallHeading = tw.h3`text-lg mb-2 font-bold`
-const Bolded = tw.p`text-gray-700 font-bold italic`
-const Italic = tw.p`text-gray-700 text-xs italic`
 
 export default function Privacy() {
   const [policy, setPolicy] = useState('');
@@ -29,20 +19,9 @@ export default function Privacy() {
 
   return (
     <div className="m-4 p-4 shadow">
-      <ReactMarkdown
-        components={{
-          p: Paragraph,
-          ul: UList,
-          ol: OList,
-          li: LI,
-          h1: MainHeading,
-          h2: SubHeading,
-          h3: SmallHeading,
-          strong: Bolded,
-          em: Italic,
-        }}>
+      <ClydeMarkdown>
         {policy}
-      </ReactMarkdown>
+      </ClydeMarkdown>
     </div>
   )
 }
