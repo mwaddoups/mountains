e888b2468174c508f765738ba83e091a6fde2c81
Mark Waddoups <mwaddoups@gmail.com>
2022-11-09 11:38:24 +0000

    Added event type

--- mountains/events/models.py
+++ mountains/events/models.py
@@ -5,9 +5,19 @@ User = get_user_model()
 
 
 class Event(models.Model):
+    class EventType(models.TextChoices):
+        SUMMER_DAY_WALK = 'SD'
+        SUMMER_WEEKEND = 'SW'
+        WINTER_DAY_WALK = 'WD'
+        WINTER_WEEKEND = 'WW'
+        CLIMBING = 'CL'
+        SOCIAL = 'SO'
+        OTHER = 'XX'
+
     title = models.CharField(max_length=100, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     event_date = models.DateTimeField(blank=False)
+    event_type = models.CharField(max_length=2, choices=EventType.choices, default=EventType.OTHER)
     description = models.TextField(blank=False)
     attendees = models.ManyToManyField(User, through='AttendingUser', blank=True)
     max_attendees = models.IntegerField(null=False, blank=False, default=0)
