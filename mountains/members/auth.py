6017e6ba403b8690cd903c8a35ca8ae443d8ffa9
Mark Waddoups <mwaddoups@gmail.com>
2024-01-08 19:42:50 +0000

    Updated to set last login time on login

--- mountains/members/auth.py
+++ mountains/members/auth.py
@@ -1,11 +1,14 @@
 import datetime
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import update_last_login
 
 def create_authentication_token(user):
     """
     Custom token handling for drfpasswordless, allows us to expire tokens
     """
     token, newly_created = Token.objects.get_or_create(user=user)
+
+    update_last_login(None, user)
 
     if not newly_created:
         # Check for expiry
