6536af838f7d489c49f7551a9af8d18193268bba
Mark Waddoups <mwaddoups@gmail.com>
2024-01-19 18:44:22 +0000

    Add last login to admin

--- mountains/members/admin.py
+++ mountains/members/admin.py
@@ -2,32 +2,51 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Experience, User
 
+
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'is_staff', 'is_superuser', 'is_approved', 'is_committee', 'is_walk_coordinator')
+    list_display = (
+        "email",
+        "is_staff",
+        "is_superuser",
+        "is_approved",
+        "is_committee",
+        "is_walk_coordinator",
+        "last_login",
+    )
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'profile_picture')}),
-        ('Personal Information', {'fields': ('first_name', 'last_name', 'mobile_number')}),
-        ('Committee Information', {'fields': ('committee_role', 'committee_bio')}),
-        ('Permissions', {'fields': (
-            'is_approved',
-            'is_committee',
-            'is_walk_coordinator',
-            'is_active',
-            'is_staff',
-            'is_superuser',
-            'groups',
-            'user_permissions',
-        )}),
-        ('Relevant Dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("email", "password", "profile_picture")}),
+        (
+            "Personal Information",
+            {"fields": ("first_name", "last_name", "mobile_number")},
+        ),
+        ("Committee Information", {"fields": ("committee_role", "committee_bio")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_approved",
+                    "is_committee",
+                    "is_walk_coordinator",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Relevant Dates", {"fields": ("last_login", "date_joined")}),
     )
 
-    search_fields = ('email',)
-    ordering = ('email',)
+    search_fields = ("email",)
+    ordering = ("email",)
     filter_horizontal = ()
 
+
 class ExperienceAdmin(admin.ModelAdmin):
-    list_display = ('user', 'activity', 'competency')
-    ordering = ('user', )
+    list_display = ("user", "activity", "competency")
+    ordering = ("user",)
+
 
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(Experience, ExperienceAdmin)
