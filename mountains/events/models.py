66d02555d322d2a0b6243d4d7a7cbdbf4a82d46a
Mark Waddoups <mwaddoups@gmail.com>
2024-01-19 16:00:57 +0000

    Finished tidying up Event backend

--- mountains/events/models.py
+++ mountains/events/models.py
@@ -1,28 +1,35 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from members.models import User
 
 
 class Event(models.Model):
     class EventType(models.TextChoices):
-        SUMMER_DAY_WALK = 'SD'
-        SUMMER_WEEKEND = 'SW'
-        WINTER_DAY_WALK = 'WD'
-        WINTER_WEEKEND = 'WW'
-        CLIMBING = 'CL'
-        SOCIAL = 'SO'
-        COMMITTEE = 'CM'
-        OTHER = 'XX'
+        SUMMER_DAY_WALK = "SD"
+        SUMMER_WEEKEND = "SW"
+        WINTER_DAY_WALK = "WD"
+        WINTER_WEEKEND = "WW"
+        CLIMBING = "CL"
+        SOCIAL = "SO"
+        COMMITTEE = "CM"
+        OTHER = "XX"
 
+    id: int
     title = models.CharField(max_length=100, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
     event_date = models.DateTimeField(blank=False)
-    event_type = models.CharField(max_length=2, choices=EventType.choices, default=EventType.OTHER)
+    event_type = models.CharField(
+        max_length=2, choices=EventType.choices, default=EventType.OTHER
+    )
     description = models.TextField(blank=False)
-    attendees = models.ManyToManyField(User, through='AttendingUser', blank=True)
+    attendees = models.ManyToManyField(User, through="AttendingUser", blank=True)
     max_attendees = models.IntegerField(null=False, blank=False, default=0)
-    organiser = models.ForeignKey(User, on_delete=models.SET_NULL, blank=False, null=True, related_name='organiser')
+    organiser = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name="organiser",
+    )
     show_popup = models.BooleanField(blank=False, null=False, default=False)
     members_only = models.BooleanField(blank=False, null=False, default=False)
     signup_open = models.BooleanField(blank=False, null=False, default=True)
@@ -33,19 +40,26 @@ class Event(models.Model):
         """
         Checks if the event is full (0 is interpreted as no limit)
         """
-        num_attendees = self.attendees.filter(attendinguser__is_waiting_list=False).count()
+        num_attendees = self.attendees.filter(
+            attendinguser__is_waiting_list=False
+        ).count()
         return self.max_attendees != 0 and num_attendees >= self.max_attendees
 
     def has_waiting_list(self) -> bool:
         """
         Returns true if either the event is full, or it already has a waiting list
         """
-        waiting_list_size = self.attendees.filter(attendinguser__is_waiting_list=True).count()
+        waiting_list_size = self.attendees.filter(
+            attendinguser__is_waiting_list=True
+        ).count()
         return self.is_full() or waiting_list_size > 0
+
 
 class AttendingUser(models.Model):
     modified_date = models.DateTimeField(auto_now=True)
-    list_join_date = models.DateTimeField(auto_now_add=True) # Gets updated manually by waiting list changes
+    list_join_date = models.DateTimeField(
+        auto_now_add=True
+    )  # Gets updated manually by waiting list changes
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     is_waiting_list = models.BooleanField(blank=False, null=False, default=False)
--- mountains/events/views.py
+++ mountains/events/views.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import datetime
 import pytz
 import itertools
@@ -11,9 +12,13 @@ from .serializers import BasicEventSerializer, EventSerializer, AttendingUserSer
 from activity.models import Activity
 from django.utils import timezone
 from django.core.mail import EmailMultiAlternatives
+from django.db.models import Count, Value
+from django.db.models.functions import Concat
+
 
 def user_allowed_edit_events(user):
     return user.is_committee or user.is_walk_coordinator
+
 
 class IsEventEditorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, user_obj):
@@ -22,21 +27,28 @@ class IsEventEditorOrReadOnly(permissions.BasePermission):
         else:
             return user_allowed_edit_events(request.user)
 
+
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [permissions.IsAdminUser | (permissions.IsAuthenticated & IsEventEditorOrReadOnly)]
+    permission_classes = [
+        permissions.IsAdminUser
+        | (permissions.IsAuthenticated & IsEventEditorOrReadOnly)
+    ]
 
     def list(self, request):
         future = datetime.datetime.combine(datetime.date.today(), datetime.time(0))
-        future_events = Event.objects.filter(event_date__gte=future, is_deleted=False).order_by('event_date')
-        past_events = Event.objects.filter(event_date__lt=future, is_deleted=False).order_by('-event_date')
+        future_events = Event.objects.filter(
+            event_date__gte=future, is_deleted=False
+        ).order_by("event_date")
+        past_events = Event.objects.filter(
+            event_date__lt=future, is_deleted=False
+        ).order_by("-event_date")
         raw_events = itertools.chain(future_events, past_events)
 
-
-        selected_id = self.request.query_params.get('selectedId')
-        limit = self.request.query_params.get('limit')
-        offset = self.request.query_params.get('offset')
+        selected_id = request.query_params.get("selectedId")
+        limit = request.query_params.get("limit")
+        offset = request.query_params.get("offset")
         if selected_id is not None:
             selected_id = int(selected_id)
             events = []
@@ -56,105 +68,137 @@ class EventViewSet(viewsets.ModelViewSet):
             # Just return future events if no params passed
             events = list(future_events)
             last_offset = len(events)
-            
 
-        serializer = self.serializer_class(events, many=True, context={'request': request})
-        return Response({
-            'results': serializer.data,
-            'last_offset': last_offset
-        })
+        serializer = self.serializer_class(
+            events, many=True, context={"request": request}
+        )
+        return Response({"results": serializer.data, "last_offset": last_offset})
 
-    @action(methods=['patch', 'post'], detail=True, permission_classes=[permissions.IsAuthenticated])
+    @action(
+        methods=["patch", "post"],
+        detail=True,
+        permission_classes=[permissions.IsAuthenticated],
+    )
     def attend(self, request, pk=None):
         event: Event = self.get_object()
         user: User = request.user
-        if request.method == 'POST' and user_allowed_edit_events(request.user):
-            user_id = request.data['userId']
+        if request.method == "POST" and user_allowed_edit_events(request.user):
+            user_id = request.data["userId"]
             user = User.objects.get(pk=user_id)
 
-        attending_user, _was_created = AttendingUser.objects.get_or_create(user=user, event=event)
+        attending_user, _was_created = AttendingUser.objects.get_or_create(
+            user=user, event=event
+        )
         if event.has_waiting_list():
             attending_user.is_waiting_list = True
             attending_user.save()
             if user != request.user:
-                Activity.objects.create(user=request.user, event=event, action=f"added {user.first_name} {user.last_name} to waiting list for")
+                Activity.objects.create(
+                    user=request.user,
+                    event=event,
+                    action=f"added {user.first_name} {user.last_name} to waiting list for",
+                )
             else:
-                Activity.objects.create(user=user, event=event, action="joined waiting list for")
+                Activity.objects.create(
+                    user=user, event=event, action="joined waiting list for"
+                )
         else:
             attending_user.is_waiting_list = False
             attending_user.save()
             if user != request.user:
-                Activity.objects.create(user=request.user, event=event, action=f"added {user.first_name} {user.last_name} to")
+                Activity.objects.create(
+                    user=request.user,
+                    event=event,
+                    action=f"added {user.first_name} {user.last_name} to",
+                )
             else:
                 Activity.objects.create(user=user, event=event, action="joined")
 
-        updated_event = EventSerializer(event, context={'request': request}) 
+        updated_event = EventSerializer(event, context={"request": request})
         return Response(updated_event.data)
 
-    @action(methods=['get'], detail=False, permission_classes=[])
+    @action(methods=["get"], detail=False, permission_classes=[])
     def upcoming(self, request):
-        today_date = datetime.datetime.combine(datetime.datetime.today(), datetime.time(0,0), tzinfo=pytz.UTC)
-        upcoming_events = Event.objects.filter(event_date__gte=today_date).order_by('event_date')[:3]
+        today_date = datetime.datetime.combine(
+            datetime.datetime.today(), datetime.time(0, 0), tzinfo=pytz.UTC
+        )
+        upcoming_events = Event.objects.filter(event_date__gte=today_date).order_by(
+            "event_date"
+        )[:3]
 
         serializer = BasicEventSerializer(upcoming_events, many=True)
         return Response(serializer.data)
 
-    @action(methods=['post'], detail=False, permission_classes=[permissions.IsAuthenticated])
+    @action(
+        methods=["post"], detail=False, permission_classes=[permissions.IsAuthenticated]
+    )
     def attendedby(self, request):
-        user_id = request.data['userId']
-        attended_events = [au.event for au in AttendingUser.objects.filter(user=user_id, is_waiting_list=False)]
-        attended_events = sorted(
-            [e for e in attended_events if e.event_date < timezone.now() + datetime.timedelta(days=1)], 
-            key=lambda e: e.event_date, reverse=True
+        """Returns list of all events attended by userId"""
+        attended_user_events = list(
+            AttendingUser.objects.filter(
+                user=request.data["userId"],
+                is_waiting_list=False,
+                event__event_date__lt=timezone.now() + datetime.timedelta(days=1),
+            ).order_by("-event__event_date")
         )
+
+        attended_events = [au.event for au in attended_user_events]
 
         serializer = BasicEventSerializer(attended_events, many=True)
         return Response(serializer.data)
 
-    @action(methods=['get'], detail=False, permission_classes=[permissions.IsAuthenticated])
+    @action(methods=["get"], detail=False, permission_classes=[IsCommittee])
     def needsmembership(self, request):
         """Returns a list of users who have attended 1 or more walks but aren't yet a member"""
-        attended_users = {}
-        for au in AttendingUser.objects.all():
-            if not au.user.is_paid and au.event.event_type in ('SD', 'SW', 'WD', 'WW') and au.event.event_date < timezone.now() and not au.is_waiting_list:
-                if au.user.id not in attended_users:
-                    attended_users[au.user.id] = {'user': au.user, 'count': 1}
-                else:
-                    attended_users[au.user.id]['count'] += 1
+        attended_users = list(
+            AttendingUser.objects.filter(
+                user__is_paid=False,
+                event__event_type__in=("SD", "SW", "WD", "WW"),
+                event__event_date__lt=timezone.now(),
+                is_waiting_list=False,
+            )
+            .annotate(
+                name=Concat(
+                    "user__first_name",
+                    Value(" "),
+                    "user__last_name",
+                )
+            )
+            .values("name")
+            .annotate(count=Count("id"))
+            .order_by("-count")
+        )
 
-        users_to_prompt = sorted([
-            {'name': d['user'].first_name + ' ' + d['user'].last_name, 'count': d['count']}
-            for d in attended_users.values()
-        ], key=lambda x: x['count'], reverse=True)
+        return Response(attended_users)
 
-
-        return Response(users_to_prompt)
-
-    @action(methods=['get'], detail=False, permission_classes=[permissions.IsAuthenticated])
+    @action(methods=["get"], detail=False, permission_classes=[IsCommittee])
     def unpaidweekends(self, request):
-        """Returns a list of users who have attended 1 or more walks but aren't yet a member"""
-        events_to_unpaid = {}
-        for au in AttendingUser.objects.all():
-            if au.event.event_type in ('SW', 'WW') and not au.event.is_deleted and au.event.event_date > timezone.now() and not au.is_trip_paid and not au.is_waiting_list:
-                event_str = au.event.event_date.strftime("%Y-%m-%d ") + au.event.title
-                if event_str not in events_to_unpaid:
-                    events_to_unpaid[event_str] = []
-                
-                user_name = au.user.first_name + ' ' + au.user.last_name
-                events_to_unpaid[event_str].append(user_name)
+        """Returns a list of events with all the users who are yet to pay"""
+        events_to_unpaid = defaultdict(list)
+        au: AttendingUser
+        for au in AttendingUser.objects.filter(
+            event__event_type__in=("SW", "WW"),
+            event__event_date__gt=timezone.now(),
+            event__is_deleted=False,
+            is_trip_paid=False,
+            is_waiting_list=False,
+        ):
+            event_str = au.event.event_date.strftime("%Y-%m-%d ") + au.event.title
+            user_name = au.user.first_name + " " + au.user.last_name
+            events_to_unpaid[event_str].append(user_name)
 
         events_to_unpaid = {k: events_to_unpaid[k] for k in sorted(events_to_unpaid)}
 
         return Response(events_to_unpaid)
 
-    @action(methods=['post'], detail=True, permission_classes = [IsEventEditorOrReadOnly])
+    @action(methods=["post"], detail=True, permission_classes=[IsEventEditorOrReadOnly])
     def reminderemail(self, request, pk=None):
         event = self.get_object()
         attendee_emails = list([u.email for u in event.attendees.all()])
 
-        email_subject = f'Reminder: CMC walk, {event.title}'
+        email_subject = f"Reminder: CMC walk, {event.title}"
 
-        email_body =  (
+        email_body = (
             f"This is a reminder that you have signed up or are on the waiting list for {event.title} on {event.event_date.strftime('%Y-%m-%d')}!\n\n"
             "If you still wish to attend, you must confirm your place on Discord in the associated walk thread."
             "Discord will also be used for any updates this week, as well as organising lifts and handing out waiting list places - so please keep an eye on it!\n\n"
@@ -162,7 +206,7 @@ class EventViewSet(viewsets.ModelViewSet):
             "If we are unable to reach you on Discord in the next couple days, we will remove you from the event.\n\n"
             "Looking forward to the walk and will hopefully see you there!"
         )
-        email_html =  (
+        email_html = (
             f"<h1>Confirm your place: {event.title}, {event.event_date.strftime('%Y-%m-%d')}</h1>"
             f"<p>This is a reminder that you have signed up or are on the waiting list for {event.title} on {event.event_date.strftime('%Y-%m-%d')}!</p>\n"
             '<p style="font-weight: bold;">If you still wish to attend, you must confirm your place on Discord in the associated walk thread.</p>\n'
@@ -182,18 +226,23 @@ class EventViewSet(viewsets.ModelViewSet):
             email.attach_alternative(email_html, "text/html")
             email.send(fail_silently=True)
 
+        return Response({"is_approved": True})
 
-        return Response({'is_approved': True})
 
 # Mixins avoid including list, which we would never want
-class AttendingUserViewSet(mixins.CreateModelMixin,
-                    mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    viewsets.GenericViewSet):
+class AttendingUserViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = AttendingUser.objects.all()
     serializer_class = AttendingUserSerializer
-    permission_classes = [permissions.IsAdminUser | (permissions.IsAuthenticated & (IsCommittee | IsWalkCo | ReadOnly))]
+    permission_classes = [
+        permissions.IsAdminUser
+        | (permissions.IsAuthenticated & (IsCommittee | IsWalkCo | ReadOnly))
+    ]
 
     def destroy(self, request, *args, **kwargs):
         """
@@ -203,11 +252,13 @@ class AttendingUserViewSet(mixins.CreateModelMixin,
 
         if att_user.user != request.user:
             Activity.objects.create(
-                user=request.user, 
-                event=att_user.event, 
-                action=f"removed {att_user.user.first_name} {att_user.user.last_name} from"
+                user=request.user,
+                event=att_user.event,
+                action=f"removed {att_user.user.first_name} {att_user.user.last_name} from",
             )
         else:
-            Activity.objects.create(user=att_user.user, event=att_user.event, action="left")
+            Activity.objects.create(
+                user=att_user.user, event=att_user.event, action="left"
+            )
 
         return super().destroy(request, *args, **kwargs)
