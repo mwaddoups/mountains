95027e98697bdf260b658b5a36281394255b8ba9
Mark Waddoups <mwaddoups@gmail.com>
2024-01-30 20:44:08 +0000

    Added modal for prompting when event is paid.

--- frontend/src/components/events/EventPaymentButton.tsx
+++ frontend/src/components/events/EventPaymentButton.tsx
@@ -1,8 +1,9 @@
-import { useResolvedPath } from "react-router-dom";
+import { useResolvedPath, useSearchParams } from "react-router-dom";
 import { AttendingUser, Event } from "../../models";
-import { Button, CancelButton } from "../base/Base";
+import { Bolded, Button, CancelButton, Paragraph } from "../base/Base";
 import { useCallback } from "react";
 import api from "../../api";
+import Modal from "../base/Modal";
 
 interface EventPaymentButtonProps {
   attUser: AttendingUser;
@@ -15,6 +16,7 @@ export default function EventPaymentButton({
 }: EventPaymentButtonProps) {
   // Ensures we strip the path of any event Id
   const resolvedPath = useResolvedPath(`../${event.id}`);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const triggerPayment = useCallback(() => {
     const return_domain = window.location.origin + resolvedPath.pathname;
@@ -31,11 +33,24 @@ export default function EventPaymentButton({
     });
   }, [resolvedPath, event, attUser]);
 
-  if (attUser.is_trip_paid) {
-    return (
-      <CancelButton className="pointer-events-none">You've paid!</CancelButton>
-    );
-  } else {
-    return <Button onClick={triggerPayment}>Pay Now</Button>;
-  }
+  return (
+    <>
+      {attUser.is_trip_paid ? (
+        <CancelButton className="pointer-events-none">
+          You've paid!
+        </CancelButton>
+      ) : (
+        <Button onClick={triggerPayment}>Pay Now</Button>
+      )}
+      {searchParams.get("success") && (
+        <Modal>
+          <Paragraph>
+            Thank you for your payment for <Bolded>{event.title}</Bolded>!
+          </Paragraph>
+          <Paragraph>Your status as paid should be updated shortly.</Paragraph>
+          <Button onClick={() => setSearchParams({})}>Return to event</Button>
+        </Modal>
+      )}
+    </>
+  );
 }
