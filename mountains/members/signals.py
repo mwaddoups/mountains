df231932cadce153fd2708fe8fe70bdfe2a176b7
Mark Waddoups <mwaddoups@gmail.com>
2024-02-19 11:37:27 +0000

    Removed new emails

--- mountains/members/signals.py
+++ mountains/members/signals.py
@@ -7,19 +7,18 @@ from members.models import User
 @receiver(post_save, sender=User)
 def email_superusers_about_new_user(instance, **kwargs):
     if not instance.is_approved and instance.first_name and instance.last_name:
-        email_body =  (
-            f"New user {instance.first_name} {instance.last_name} ({instance.email}) has been auto-approved!\n\n"
-        )
+        email_body = f"New user {instance.first_name} {instance.last_name} ({instance.email}) has been auto-approved!\n\n"
 
         instance.is_approved = True
         instance.save()
 
         superusers = User.objects.filter(is_superuser=True)
 
-        send_mail(
-            "Clyde Mountaineering Club - New User",
-            email_body,
-            "noreply@clydemc.org",
-            ["admin@clydemc.org"],
-            fail_silently=True,
-        )
+        # NOTE: New user emails are currently disabled. To enable, uncomment here.
+        # send_mail(
+        #     "Clyde Mountaineering Club - New User",
+        #     email_body,
+        #     "noreply@clydemc.org",
+        #     ["admin@clydemc.org"],
+        #     fail_silently=True,
+        # )
